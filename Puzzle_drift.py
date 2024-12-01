b10d32d4a599db70b78c49214d9286ce1ce8f087
urooba2634 <uroobaubaid2005@gmail.com>
2024-12-01 23:48:09 +0500

    Update Puzzle_drift.py

--- Puzzle_drift.py
+++ Puzzle_drift.py
@@ -66,3 +66,220 @@ abstract class PuzzlePanel extends JPanel {
 
     public abstract boolean checkWinCondition();
 }
+
+Class Player1Panel extends PuzzlePanel {
+
+    GamePanel gamePanel;
+
+
+
+    Public Player1Panel(String name, GamePanel gamePanel) {
+
+        playerName = name;
+
+        this.gamePanel = gamePanel;
+
+        setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.BLUE, 3), playerName));
+
+    }
+
+
+
+    @Override
+
+    Public void initializeGame(BufferedImage image, int gridSize) {
+
+        removeAll();
+
+        setLayout(new GridLayout(gridSize, gridSize, 0, 0));
+
+        buttons = new JButton[gridSize][gridSize];
+
+        fullImage = image;
+
+
+
+        ArrayList<Integer> tiles = new ArrayList<>();
+
+        For (int i = 0; i < gridSize * gridSize; i++) {
+
+            Tiles.add(i);
+
+        }
+
+        Collections.shuffle(tiles);
+
+
+
+        Int panelSize = 778;
+
+        setPreferredSize(new Dimension(panelSize, panelSize));
+
+        int buttonSize = panelSize / gridSize;
+
+
+
+        for (int i = 0; i < gridSize; i++) {
+
+            for (int j = 0; j < gridSize; j++) {
+
+                int tile = tiles.get(i * gridSize + j);
+
+                buttons[i][j] = new JButton();
+
+                buttons[i][j].setPreferredSize(new Dimension(buttonSize, buttonSize));
+
+                buttons[i][j].setMargin(new Insets(0, 0, 0, 0));
+
+                buttons[i][j].setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+                if (tile == gridSize * gridSize – 1) {
+
+                    buttons[i][j].setText(“”);
+
+                    emptyRow = i;
+
+                    emptyCol = j;
+
+                } else {
+
+                    BufferedImage tileImage = image.getSubimage(
+
+                            (tile % gridSize) * (image.getWidth() / gridSize),
+
+                            (tile / gridSize) * (image.getHeight() / gridSize),
+
+                            Image.getWidth() / gridSize,
+
+                            Image.getHeight() / gridSize
+
+                    );
+
+                    Image scaledImage = tileImage.getScaledInstance(buttonSize, buttonSize, Image.SCALE_SMOOTH);
+
+                    Buttons[i][j].setIcon(new ImageIcon(scaledImage));
+
+                    Buttons[i][j].setText(String.valueOf(tile + 1));
+
+                    Buttons[i][j].setHorizontalTextPosition(JButton.CENTER);
+
+                    Buttons[i][j].setVerticalTextPosition(JButton.CENTER);
+
+                    Buttons[i][j].setForeground(new Color(0, 0, 0, 0));
+
+                }
+
+                Final int finalI = i;
+
+                Final int finalJ = j;
+
+                Buttons[i][j].addActionListener(e -> {
+
+                    If (moveTile(finalI, finalJ)) {
+
+                        gamePanel.playMoveSound();
+
+                        gamePanel.updateMovesLabels();
+
+                        gamePanel.checkWinCondition();
+
+                    }
+
+                });
+
+                Add(buttons[i][j]);
+
+            }
+
+        }
+
+        Revalidate();
+
+        Repaint();
+
+    }
+
+
+
+    @Override
+
+    Protected JButton[][] getButtons() {
+
+        Return buttons;
+
+    }
+
+
+
+    @Override
+
+    Public boolean checkWinCondition() {
+
+        Int gridSize = buttons.length;
+
+        For (int i = 0; i < gridSize; i++) {
+
+            For (int j = 0; j < gridSize; j++) {
+
+                Int expectedTile = i * gridSize + j;
+
+                If (expectedTile < gridSize * gridSize – 1) {
+
+                    If (!buttons[i][j].getText().equals(String.valueOf(expectedTile + 1))) {
+
+                        Return false;
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        Return true;
+
+    }
+
+
+
+    @Override
+
+    Protected void paintComponent(Graphics g) {
+
+        Super.paintComponent(g);
+
+        If (!gamePanel.isTwoPlayerMode) {
+
+            Try {
+
+                BufferedImage backgroundImage = ImageIO.read(new File(“C:\\Users\\Nuzhat\\Downloads\\b2.jpg”));
+
+                g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+
+            }
+
+        }
+
+    }
+
+}
+
+
+
+Class Player2Panel extends Player1Panel {
+
+    Public Player2Panel(String name, GamePanel gamePanel) {
+
+        Super(name, gamePanel);
+
+        setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.RED, 3), playerName));
+
+    }
+
+}
+
